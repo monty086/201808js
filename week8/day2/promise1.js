343503180cb298dbe6519fa6c019849590ee91c8
yuanmeng <372909885@qq.com>
2018-10-14 10:39:40 +0800

    promise1

---
+++ week8/day2/promise1.js
@@ -0,0 +1,43 @@
+class Promise {
+    constructor(excutor) {
+        // Promise构造函数当中的内容
+        this.status = 'pending';
+        this.value = undefined;
+        // 创建两个容器成功和失败
+        this.fulfilledAry = [] ;
+        this.rejectedAry = [] ;
+
+        // 成功之后执行的方法
+        let resolve = result => {
+            // 添加异步代码，进行异步操作
+            let timer = setTimeout(() => {
+                clearTimeout(timer);
+                if (this.status === 'pending') {
+                    this.status = 'fulfilled';
+                    this.value = result;
+                    this.fulfilledAry.forEach(item=>item())
+                    // item =>每一个匿名【匿名函数执行=》成功的回调函数就会执行】
+                }
+            }, 0);
+
+        }
+        // 失败以后执行的方法
+        let reject = reason => {
+            let timer = setTimeout(() => {
+                clearTimeout(timer);
+                if (this.status === 'pending') {
+                    this.status = 'rejected';
+                    this.value = reason;
+                    this.rejectedAry.forEach(item=>item())
+                }
+            }, 0);
+
+        }
+        excutor(resolve, reject)
+    }
+    // promise原型上的方法
+    then(fulfilledCB,rejectedCB){
+        this.fulfilledAry.push(()=>{fulfilledCB(this.value)});
+        this.rejectedAry.push(()=>{rejectedCB(this.value)})
+    }
+}
